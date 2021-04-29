a12ffa81ea3b75ef52584bd087819d907fe33fd2
siyingxie <35740441+siyingxie@users.noreply.github.com>
2021-04-29 15:44:41 +0200

    modified comments

--- code/cvmvnn.m
+++ code/cvmvnn.m
@@ -1,12 +1,25 @@
 function [data_whitened]= cvmvnn (data, trialIndex)
-% Multivariate noise normalization
+% Multivariate noise normalization, where the error covariance between
+% different sensors is considered, and sensors with high noise
+% levels (i.e., unreliable sensors) would be downweighted and sensors with
+% low noise levels (i.e., reliable sensors) be emphasized.
 
-%calculate channel Epsilon only on training data
+% Takes inputs: data - 4D array in [conition x trial x sensor x time points]
+% format
+% trialIndex - a single column array, indexing the trials used for
+% calculating the covariance matrix. We usually use the trials in 
+% the training data set.
+
+% Gives output: data_whitened - 4D array in [conition x trial x sensor x time points]
+% format
+
+%% calculate sensor Epsilon
 sz = size(data);
-E = NaN(sz(4), sz(1), sz(3), sz(3)); %epsilon of all channels
+%epsilon of all sensors
+E = NaN(sz(4), sz(1), sz(3), sz(3)); 
 
-for t = 1:sz(4)
-    for c = 1:sz(1)
+for t = 1:sz(4) %loop for time points
+    for c = 1:sz(1) %loop for conditions
         
         X = squeeze(data(c, trialIndex, :, t));
         
@@ -15,19 +28,27 @@ for t = 1:sz(4)
     end
     
 end
-EE = squeeze(mean(mean(E, 1), 2)); %average by conditions and time points
+
+%average by conditions and time points
+EE = squeeze(mean(mean(E, 1), 2)); 
 clear E
-IE = EE^(-0.5); %invert E
+%invert E
+IE = EE^(-0.5); 
 
 
-% apply channel Epsilon on all data
-data_whitened = NaN(sz); %pre-allocate matrix
-for t = 1:sz(4)
-    for c = 1:sz(1)
-        for l = 1:sz(2)
+%% apply channel Epsilon on all data
+
+%pre-allocate matrix
+data_whitened = NaN(sz);
+for t = 1:sz(4) %loop for time points
+    for c = 1:sz(1) %loop for conditions
+        for l = 1:sz(2) %loop for trials
             
             X = squeeze(data(c, l, :, t))';
-            W = X * IE; %whiten
+            
+            %whiten
+            W = X * IE;
+            
             data_whitened(c, l, :, t) = W;
             
         end
