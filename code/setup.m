4380b37e5875a60d5799f84d20afd0e0140dd380
siyingxie <seeing.xie@gmail.com>
2022-10-31 11:38:45 +0100

    added no input defined

--- code/setup.m
+++ code/setup.m
@@ -4,19 +4,22 @@ function setup(downloadseed)
 
 path0 = strrep(which('setup.m'),[filesep 'code' filesep 'setup.m'],'');
 
-% Download the dataset if it has not already been downloaded.
-% (If you like, you may manually download the dataset from:
-%    https://osf.io/ruxfg/?view_only=919a62ecab944e99a80b9a467951d2e1)
-load(fullfile(path0,'data', 'dataseed.mat'), 'osfURL');
-
-for iseed = downloadseed
-    if ~exist(fullfile(path0,'data',osfURL(iseed).file),'file')
-        fprintf('Downloading %s (please be patient).\n',osfURL(iseed).file);
-        urlwrite(osfURL(iseed).link, fullfile(path0,'data',osfURL(iseed).file));
-        fprintf('Downloading is done!\n');
-    end
-end
-
 % Add VCR_infant the MATLAB path (in case the user has not already done so).
 addpath(genpath(path0));
+
+if ~isempty(downloadseed)
+    % Download the dataset if it has not already been downloaded.
+    % (If you like, you may manually download the dataset from:
+    %    https://osf.io/ruxfg/?view_only=919a62ecab944e99a80b9a467951d2e1)
+    load(fullfile(path0,'data', 'dataseed.mat'), 'osfURL');
+    
+    for iseed = downloadseed
+        if ~exist(fullfile(path0,'data',osfURL(iseed).file),'file')
+            fprintf('Downloading %s (please be patient).\n',osfURL(iseed).file);
+            urlwrite(osfURL(iseed).link, fullfile(path0,'data',osfURL(iseed).file));
+            fprintf('Downloading is done!\n');
+        end
+    end
+    
+end
 end
